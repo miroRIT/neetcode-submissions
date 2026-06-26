5b7e6fac302cc9169a9daa7ec6848a8b2774b02c
Aayush gupta <ayush.98g@gmail.com>
2026-06-26 11:33:42 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length != t.length)
+            return false
+        
+        let dict = {};
+        for (let i = 0; i < s.length; i++){
+            dict[s[i]] = (dict[s[i]] || 0) + 1
+            dict[t[i]] = (dict[t[i]] || 0) - 1
+        }
+        for(let elem in dict){
+            if(dict[elem] != 0)
+                return false
+        }
+        return true
+    }
+}
