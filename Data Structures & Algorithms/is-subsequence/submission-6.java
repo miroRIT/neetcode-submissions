15767ebaaa46b6921d00a72c7dc1b0ddeaa271a1
Aayush gupta <ayush.98g@gmail.com>
2026-06-27 16:33:57 +0530

    Add: is-subsequence - submission-6

---
+++ Data Structures & Algorithms/is-subsequence/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty()){
+            return true;
+        }
+        int s_ind = 0, t_ind = 0;
+        while (t_ind < t.length()){
+            if (s.charAt(s_ind) == t.charAt(t_ind)){
+                s_ind++;
+            }
+            
+            if (s_ind == s.length()){
+                return true;
+            }
+
+            t_ind++;
+        }
+        return false;
+    }
+}
