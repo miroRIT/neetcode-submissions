205506556c312e6a7834e4e7df9969ef5e8763af
Aayush gupta <ayush.98g@gmail.com>
2026-06-27 16:38:24 +0530

    Add: is-subsequence - submission-8

---
+++ Data Structures & Algorithms/is-subsequence/submission-8.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s: string, t: string): boolean {
+        if (!s) return true
+        
+        let t_ind = 0, s_ind = 0
+
+        while (t_ind < t.length){
+            if (s[s_ind] == t[t_ind]){
+                s_ind++
+            }
+
+            if(s_ind == s.length) return true
+
+            t_ind++
+        }
+
+        return false
+    }
+}
