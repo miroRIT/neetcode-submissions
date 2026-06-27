6538cb09c6a20a1e091345bdc62badfc4b9a66f8
Aayush gupta <ayush.98g@gmail.com>
2026-06-28 05:11:09 +0530

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let max_num = 0, count = 0
+        nums.forEach((item, index) => {
+            if (item == 1){
+                count++
+            }else{
+                count = 0
+            }
+            max_num = Math.max(count, max_num)
+        });
+        return max_num
+    }
+}
