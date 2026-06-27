6a85df358c119b6a21b720f2ebe5510a37881903
Aayush gupta <ayush.98g@gmail.com>
2026-06-27 17:07:15 +0530

    Add: append-characters-to-string-to-make-subsequence - submission-2

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-2.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s: string, t: string): number {
+        if (!t)
+            return 0
+        let s_ind = 0, t_ind = 0
+        while (s_ind < s.length){
+            if (t[t_ind] == s[s_ind])
+                t_ind += 1
+             
+            if (t_ind == t.length)
+                return 0
+
+            s_ind += 1
+        }
+        return t.length - t_ind
+    }
+}
