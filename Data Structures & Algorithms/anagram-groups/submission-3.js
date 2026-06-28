558581917b9d3d7d56e294c58312982cf041515c
Aayush gupta <ayush.98g@gmail.com>
2026-06-28 09:19:51 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let res = new Map();
+        for (const s of strs){
+            const count = new Array(26).fill(0);
+
+            for (let i = 0; i < s.length; i++){
+                count[s.charCodeAt(i) - 97]++
+            }
+
+            const key = count.join('#');
+            // If the key doesn't exist, initialize it
+            if (!res.has(key)) {
+                res.set(key, []);
+            }
+            
+            // Add the current string to the corresponding list
+            res.get(key).push(s);
+        }
+        return Array.from(res.values());
+    }
+}
