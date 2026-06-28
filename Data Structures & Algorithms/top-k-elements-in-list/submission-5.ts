feaca84f920127ea24aba7a16d104ff053f05c17
Aayush gupta <ayush.98g@gmail.com>
2026-06-28 10:52:22 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.ts
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        let res: Map<number, number> = new Map()
+        nums.forEach(i => res.set(i, (res.get(i) || 0) + 1))
+        return Array.from(res.entries()).sort((a, b) => b[1] - a[1]).slice(0, k).map(([num]) => num)
+    }
+}
