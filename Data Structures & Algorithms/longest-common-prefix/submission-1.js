6ea9a583893d1e3cc2e3b8538bc2e033bb430e7d
Aayush gupta <ayush.98g@gmail.com>
2026-06-28 08:12:24 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        strs = strs.sort()
+        let cmn_pre = strs[0]
+        for (let i = 1; i < strs.length; i++){
+            for (let j = 0; j < cmn_pre.length; j++){
+                if (cmn_pre[j] != strs[i][j]){
+                    cmn_pre = cmn_pre.substring(0,j)
+                    break
+                }
+            }
+        }
+        return cmn_pre
+    }
+}
