bd99e6a897946187886c6eee201ca1ea1b00b978
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 11:44:54 +0530

    Add: counting-bits - submission-4

---
+++ Data Structures & Algorithms/counting-bits/submission-4.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        let dp = new Array(n + 1).fill(0);
+        for (let i = 1; i <= n; i++)
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
+    }
+}
