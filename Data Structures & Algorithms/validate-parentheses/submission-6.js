3c215a3aa9fc02aff2e9bf6acd1177ab317e26ad
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 07:50:12 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stk = [];
+        for (let c of s){
+            if (['[', '{', '('].includes(c))
+                stk.push(c)
+            else{
+                if (stk.length == 0) return false
+                let brak = stk.pop()
+                if ((c == ']' && brak != '[') || (c == '}' && brak != '{') || (c == ')' && brak != '(')){
+                    return false;
+                }
+            }
+        }
+        return stk.length == 0 ? true : false
+    }
+}
