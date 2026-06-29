2f2de2278c7260d6b09bdbea530142b24f451870
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 11:20:13 +0530

    Add: number-of-one-bits - submission-2

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-2.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n: number): number {
+        let weight = 0
+        while (n){
+            n = n & (n - 1)
+            weight++
+        }
+        return weight
+    }
+}
