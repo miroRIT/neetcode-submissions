31ccc4f4f1b00ea0f7b03b85744e4e1343f57afa
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 15:45:54 +0530

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        let n = stones.length
+        if (n == 0) return 0
+        if(n == 1) return stones[0]
+        stones.sort((a, b) => a - b)
+        let i0 = stones.pop()
+        let i1 = stones.pop()
+        if (i0 != i1) stones.push(i0 - i1)
+        return this.lastStoneWeight(stones)
+    }
+}
