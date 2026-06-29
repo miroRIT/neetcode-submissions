9b9d03a36d5e48e01443296acab0a0c003be1bdc
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 12:03:52 +0530

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        let max = 0;
+        for(let i = 0; i < nums.length; i++){
+            if (i > max) return false
+
+            max = Math.max(max, i + nums[i])
+
+            if (max >= nums.length - 1) return true
+        }
+        return true
+    }
+}
