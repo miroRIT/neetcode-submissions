f383cf177cbaaff615af0b19fed00582fb12492b
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 07:13:45 +0530

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins, amount) {
+        let dp = Array(amount + 1).fill(amount + 1);
+
+        dp[0] = 0;
+
+        for (let a = 1; a <= amount; a++) {
+            for (let coin of coins) {
+                if (a - coin >= 0) dp[a] = Math.min(dp[a], 1 + dp[a - coin]);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
