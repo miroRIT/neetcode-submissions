e384c94804cfa98003ca6d3e0c2b27e989c36eb1
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 20:40:54 +0530

    Add: balanced-binary-tree - submission-4

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-4.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        const getDepth = (root) => {
+            if (root === null) return 0
+            let left = getDepth(root.left)
+            if (left == -1) return -1
+            let right = getDepth(root.right)
+            if (right == -1) return -1
+
+            if (Math.abs(right - left) > 1) return -1
+
+            return Math.max(right, left) + 1
+        }
+        return (getDepth(root) != -1)
+    }
+}
