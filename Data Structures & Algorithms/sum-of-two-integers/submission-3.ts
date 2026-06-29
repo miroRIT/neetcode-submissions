dd76648adfd59c4bae1cd3bfc7ae56137eaedc74
Aayush gupta <ayush.98g@gmail.com>
2026-06-29 09:42:34 +0530

    Add: sum-of-two-integers - submission-3

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    getSum(a: number, b: number): number {
+        while (b != 0){
+            let carry = (a & b) << 1;
+
+            a = a ^ b
+
+            b = carry
+        }
+
+        return a;
+    }
+}
