375756fab6c01e3eb7293d46e2b46abd64af421b
Aayush gupta <ayush.98g@gmail.com>
2026-06-30 12:06:54 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+        for (let s of strs) res += s.length + "#" + s;
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let n = str.length;
+        let i = 0;
+        let res = [];
+        while (i < n) {
+            let j = i;
+            while (str[j] !== "#") j++;
+            let length = parseInt(str.substring(i, j));
+            i = j + 1;
+            j = i + length;
+            res.push(str.substring(i, j));
+            i = j;
+        }
+        return res;
+    }
+}
