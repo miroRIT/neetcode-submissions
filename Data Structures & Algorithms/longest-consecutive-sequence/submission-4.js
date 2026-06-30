3b9c597cbac8209fae67b04b3dfa898994b6e84d
Aayush gupta <ayush.98g@gmail.com>
2026-06-30 08:52:49 +0530

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let streak = 0;
+        let num_set = new Set(nums);
+
+        for (let i of nums) {
+            if (!num_set.has(i - 1)) {
+                let curr = i;
+                let curr_streak = 1;
+                while (num_set.has(curr + 1)) {
+                    curr++;
+                    curr_streak++;
+                }
+                streak = Math.max(streak, curr_streak);
+            }
+        }
+        return streak;
+    }
+}
