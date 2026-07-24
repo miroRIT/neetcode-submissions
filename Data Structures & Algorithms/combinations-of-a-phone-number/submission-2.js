09119c794de9631745833fb7a83dd7f2c3ebaae1
Aayush gupta <ayush.98g@gmail.com>
2026-07-24 15:01:54 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} digits
+     * @return {string[]}
+     */
+    letterCombinations(digits) {
+        if (digits.length === 0) return [];
+        const digitToChar = {
+            2: 'abc',
+            3: 'def',
+            4: 'ghi',
+            5: 'jkl',
+            6: 'mno',
+            7: 'qprs',
+            8: 'tuv',
+            9: 'wxyz',
+        };
+        let res = [];
+
+        function backtrack(i, curStr) {
+            if (curStr.length === digits.length) {
+                res.push(curStr);
+                return;
+            }
+            for (const c of digitToChar[digits[i]]) backtrack(i + 1, curStr + c);
+        }
+
+        backtrack(0, '');
+        return res;
+    }
+}
