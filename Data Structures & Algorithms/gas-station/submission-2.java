99a1b19cb1904d9581e341e67ae308cf42a80289
Aayush gupta <ayush.98g@gmail.com>
2026-08-11 03:27:48 +0530

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (Arrays.stream(gas).sum() < Arrays.stream(cost).sum())
+            return -1;
+
+        int tank = 0, start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
+            }
+        }
+        return start;
+    }
+}
