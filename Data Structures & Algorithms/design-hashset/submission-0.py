515b9002c29f69f072e8cb602e1136f3aecc6bfc
Aayush gupta <ayush.98g@gmail.com>
2026-08-23 14:51:21 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,24 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.arr = [0] * 31251
+
+    def add(self, key: int) -> None:
+        self.arr[key // 32] |= self.getMask(key)
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.arr[key // 32] ^= self.getMask(key)
+
+    def contains(self, key: int) -> bool:
+        return self.arr[key // 32] & self.getMask(key) != 0
+        
+    def getMask(self, key: int) -> int:
+        return 1 << (key % 32)
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
