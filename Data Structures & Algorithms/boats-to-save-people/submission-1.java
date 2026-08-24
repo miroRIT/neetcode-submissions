8916bbaa5a0999483d63141313ca7e740a1dfa55
Aayush gupta <ayush.98g@gmail.com>
2026-08-24 17:40:13 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = Arrays.stream(people).max().getAsInt();
+        int[] count = new int[n + 1];
+        for (int p : people) count[p]++;
+
+        int idx = 0, i = 1;
+        while (idx < people.length) {
+            while (count[i] == 0) i++;
+            people[idx++] = i;
+            count[i]--;
+        }
+
+        int res = 0, l = 0, r = people.length - 1;
+        while (l <= r) {
+            int remain = limit - people[r--];
+            res++;
+            if (l <= r && remain >= people[l])
+                l++;
+        }
+
+        return res;
+    }
+}
