6000c6baff525dd9b7099ef29026fbbb23bbf9ab
Aayush gupta <ayush.98g@gmail.com>
2026-08-24 21:59:45 +0530

    Add: asteroid-collision - submission-4

---
+++ Data Structures & Algorithms/asteroid-collision/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int j = -1;
+
+        for (int a : asteroids){
+            while (j >= 0 && asteroids[j] > 0 && a < 0){
+                if (asteroids[j] > Math.abs(a)){
+                    a = 0;
+                    break;
+                }else if (asteroids[j] == Math.abs(a)){
+                    a = 0;
+                    j--;
+                    break;
+                }else j--;
+            }
+            if (a != 0)
+                asteroids[++j] = a;
+        }
+
+        return Arrays.copyOfRange(asteroids, 0, j + 1);
+    }
+}
