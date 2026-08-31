6b136a04865da3685dce4058ab4bbf96f3e8584c
Aayush gupta <ayush.98g@gmail.com>
2026-09-01 05:19:25 +0530

    Add: transpose-matrix - submission-1

---
+++ Data Structures & Algorithms/transpose-matrix/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        int[][] res = new int[m][n];
+
+        for (int r = 0; r < n; r++)
+            for (int c = 0; c < m; c++) res[c][r] = matrix[r][c];
+
+        return res;
+    }
+}
