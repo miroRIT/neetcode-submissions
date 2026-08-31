7dfb0da5e254d8efd0b4ad4ce75fee555ceb24b0
Aayush gupta <ayush.98g@gmail.com>
2026-09-01 03:11:48 +0530

    Add: find-the-town-judge - submission-2

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] delta = new int[n + 1];
+
+        for (int[] t : trust) {
+            delta[t[0]]--;
+            delta[t[1]]++;
+        }
+
+        for (int i = 1; i < n + 1; i++)
+            if (delta[i] == n - 1)
+                return i;
+
+        return -1;
+    }
+}
