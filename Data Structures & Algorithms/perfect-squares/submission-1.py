a410a24e0d80063532b96897906e8b778ef0e947
Aayush gupta <ayush.98g@gmail.com>
2026-09-01 14:07:14 +0530

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        while n % 4 == 0:
+            n //= 4
+
+        if n % 8 == 7:
+            return 4
+
+        def isSquare(num):
+            s = int(math.sqrt(num))
+            return s * s == num
+
+        if isSquare(n):
+            return 1
+
+        i = 1
+
+        while i * i < n:
+            if isSquare(n - i * i):
+                return 2
+            i += 1
+
+        return 3
