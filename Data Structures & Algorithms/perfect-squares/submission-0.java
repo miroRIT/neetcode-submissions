6a9403c1d6ffabebe634e552b7dfc438bb1ff1ee
Aayush gupta <ayush.98g@gmail.com>
2026-09-01 14:04:01 +0530

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSquares(int n) {
+        while (n % 4 == 0) n /= 4;
+
+        if (n % 8 == 7)
+            return 4;
+
+        if (isSquare(n))
+            return 1;
+
+        for (int i = 1; i * i <= n; i++)
+            if (isSquare(n - i * i))
+                return 2;
+
+        return 3;
+    }
+
+    private boolean isSquare(int num) {
+        int s = (int) Math.sqrt(num);
+        return s * s == num;
+    }
+}
