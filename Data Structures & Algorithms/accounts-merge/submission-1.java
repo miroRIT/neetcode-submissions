bf77b1ada52ac81b6f6770ce794f548c95e4a0b6
Aayush gupta <ayush.98g@gmail.com>
2026-09-17 11:37:21 +0530

    Add: accounts-merge - submission-1

---
+++ Data Structures & Algorithms/accounts-merge/submission-1.java
@@ -0,0 +1,77 @@
+class UnionFind {
+    private int[] par;
+    private int[] rank;
+
+    public UnionFind(int n) {
+        par = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (x != par[x])
+            par[x] = find(par[x]);
+        return par[x];
+    }
+
+    public boolean union(int x1, int x2) {
+        int p1 = find(x1);
+        int p2 = find(x2);
+        if (p1 == p2)
+            return false;
+
+        if (rank[p1] > rank[p2]) {
+            par[p2] = p1;
+            rank[p1] += rank[p2];
+        } else {
+            par[p1] = p2;
+            rank[p2] += rank[p1];
+        }
+
+        return true;
+    }
+}
+
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        int n = accounts.size();
+        UnionFind uf = new UnionFind(n);
+        Map<String, Integer> emailToAcc = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            List<String> account = accounts.get(i);
+            for (int j = 1; j < account.size(); j++) {
+                String email = account.get(j);
+                if (emailToAcc.containsKey(email))
+                    uf.union(i, emailToAcc.get(email));
+                else
+                    emailToAcc.put(email, i);
+            }
+        }
+
+        Map<Integer, List<String>> emailGroup = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : emailToAcc.entrySet()) {
+            String email = entry.getKey();
+            int accId = entry.getValue();
+            int leader = uf.find(accId);
+            emailGroup.putIfAbsent(leader, new ArrayList<>());
+            emailGroup.get(leader).add(email);
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        for (Map.Entry<Integer, List<String>> entry : emailGroup.entrySet()) {
+            int accId = entry.getKey();
+            List<String> emails = entry.getValue();
+            Collections.sort(emails);
+            List<String> merged = new ArrayList<>();
+            merged.add(accounts.get(accId).get(0));
+            merged.addAll(emails);
+            res.add(merged);
+        }
+
+        return res;
+    }
+}
